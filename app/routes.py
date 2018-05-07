d8470ec5e57fcef75e3924b116b21c5747c4e436
reda-bel <36329830+reda-bel@users.noreply.github.com>
2018-05-07 15:34:01 +0200

    1.01

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from app import app
 import recommendation
 @app.route('/')
 
-@app.route('/todo/rec/<keyword>', methods=['GET'])
+@app.route('/rec/<keyword>', methods=['GET'])
 def get_movie(keyword):
     rec=recommendation.get_recommendations(keyword)
     return jsonify('Recommended Movies:{}'.format(rec))
